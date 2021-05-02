45d4dcd1cc81aaf900b2beeda403b1ed7660325e
bhavjotsodhi <sodhi.bhavjot@gmail.com>
2021-05-02 10:51:50 +0530

    update

--- dashp.py
+++ dashp.py
@@ -16,11 +16,15 @@ JupyterDash.infer_jupyter_proxy_config()
 app.config.suppress_callback_exceptions = True
 
 # Read the airline data into pandas dataframe
-airline_data =  pd.read_csv('/Users/bhavjot/Downloads/airline_2m.csv', 
+# airline_data =  pd.read_csv('/Users/bhavjot/Downloads/airline_2m.csv', 
+#                             encoding = "ISO-8859-1",
+#                             dtype={'Div1Airport': str, 'Div1TailNum': str, 
+#                                    'Div2Airport': str, 'Div2TailNum': str})
+
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
                             encoding = "ISO-8859-1",
                             dtype={'Div1Airport': str, 'Div1TailNum': str, 
-                                   'Div2Airport': str, 'Div2TailNum': str})
-
+                                   'Div2Airport': str, 'Div2TailNum': str})   
                                    
 # List of years 
 year_list = [i for i in range(2005, 2021, 1)]
